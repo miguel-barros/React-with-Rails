23f1394182f2786ae2b4dd9f73c5acec8cdd8bed
Miguel-Barros <eomiguelbarros@gmail.com>
2023-04-28 10:26:42 -0300

    feat: creating toDo simple CRUD

--- toDo-Api/db/schema.rb
+++ toDo-Api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_131920) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_132406) do
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.string "description"
