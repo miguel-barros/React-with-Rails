a249500a4b1519528851756e7c2e61ae76542a6e
Miguel-Barros <eomiguelbarros@gmail.com>
2023-05-02 11:55:22 -0300

    perf: improving design of project

--- todo-app/src/components/todo.tsx
+++ todo-app/src/components/todo.tsx
@@ -44,15 +44,15 @@ export default function Todo() {
     }, [refresh])
 
     return (
-        <div className='w-full h-full mt-5 flex items-center justify-center flex-col'>
+        <div className='w-full h-full mt-5 flex items-center flex-col'>
             <div className='w-7/12 flex items-center justify-between mb-5'>
-                <input type="text" className='border border-gray-200 rounded-xl py-1 px-4 w-6/12 mr-3 outline-none' placeholder='New todo name' onChange={(e) => setNewTodo({ ...newTodo, name: e.target.value })} value={newTodo.name} />
-                <input type="text" className='border border-gray-200 rounded-xl py-1 px-4 w-full mr-3 outline-none' placeholder='New todo description' onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })} value={newTodo.description} />
+                <input type="text" className='bg-[#2b2b2b] rounded-xl py-1 px-4 w-6/12 mr-3 outline-none' placeholder='New todo name' onChange={(e) => setNewTodo({ ...newTodo, name: e.target.value })} value={newTodo.name} />
+                <input type="text" className='bg-[#2b2b2b] rounded-xl py-1 px-4 w-full mr-3 outline-none' placeholder='New todo description' onChange={(e) => setNewTodo({ ...newTodo, description: e.target.value })} value={newTodo.description} />
                 <button className='text-white bg-green-500 py-1 px-6 text-sm rounded-xl hover:bg-green-600 transition ease-in' onClick={() => createTodo(newTodo)}>Create</button>
             </div>
             {alert.show && <Alert severity={alert.type} onClose={() => setAlert({ ...alert, show: false })} >{alert.message}</Alert>}
             <table className='w-10/12'>
-                <thead className='text-sm border-b-2 border-gray-200'>
+                <thead className='text-sm border-b-2 border-[#2b2b2b]'>
                     <tr className=''>
                         <th className='w-1/12'>Status</th>
                         <th className='w-3/12'>Name</th>
@@ -62,18 +62,20 @@ export default function Todo() {
                 </thead>
                 <tbody>
                     {todos.map((todo: any) => (
-                        <tr key={todo.id} >
-                            <td className='flex items-center justify-center'>{todo.status ? (
-                                <Icon icon="mdi:checkbox-marked" className='text-green-500 w-8 h-8 cursor-pointer' onClick={() => {
-                                    TodoController.update({ ...todo, status: false })
-                                    setRefresh(!refresh)
-                                }} />
-                            ) : (
-                                <Icon icon="mdi:clock" className='text-yellow-500  w-8 h-8 cursor-pointer' onClick={() => {
-                                    TodoController.update({ ...todo, status: true })
-                                    setRefresh(!refresh)
-                                }} />
-                            )}</td>
+                        <tr key={todo.id}>
+                            < td className='flex items-center justify-center' > {
+                                todo.status ? (
+                                    <Icon icon="mdi:checkbox-marked" className='text-green-500 w-8 h-8 cursor-pointer' onClick={() => {
+                                        TodoController.update({ ...todo, status: false })
+                                        setRefresh(!refresh)
+                                    }} />
+                                ) : (
+                                    <Icon icon="mdi:clock" className='text-yellow-500  w-8 h-8 cursor-pointer' onClick={() => {
+                                        TodoController.update({ ...todo, status: true })
+                                        setRefresh(!refresh)
+                                    }} />
+                                )
+                            }</td>
                             <td>{todo.status ?
                                 <p className='opacity-50'><s>{todo.name}</s></p> : <p>{todo.name}</p>
                             }</td>
@@ -97,7 +99,7 @@ export default function Todo() {
                         </tr>
                     ))}
                 </tbody>
-            </table>
+            </table >
         </div >
     )
 }
--- todo-app/src/pages/index.tsx
+++ todo-app/src/pages/index.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 
 export default function Index() {
   return (
-    <div className='flex items-center justify-center'>
+    <div className='w-screen h-screen flex items-center justify-center bg-[#131516] text-[#fcfcfc]'>
       <Head>
         <title>Todo App</title>
       </Head>
-      <main className='w-full rounded-lg flex items-center justify-center flex-col mt-10'>
+      <main className='w-11/12 h-5/6 rounded-lg flex items-center justify-center flex-col mt-5 bg-[#131516]'>
         <h1 className='text-4xl font-bold'>ToDo list app</h1>
-        <p className='text-sm text-gray-700'>Your to-do list is:</p>
+        <p className='text-sm text-gray-400'>Your to-do list is:</p>
         <Todo />
       </main>
     </div>
